c11fa68a22f23ff697d014e77b5f16bec8ab853f
dengqiyuan <dengqiyuan@sina.com>
2017-12-15 15:43:11 +0800

    使用VBO和VAO绘制三角形

--- OpenGLLearning001/OpenGLLearning001/OpenGLView.m
+++ OpenGLLearning001/OpenGLLearning001/OpenGLView.m
@@ -148,6 +148,35 @@
     
 }
 
+-(void)setupVbo
+{
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+    GLuint VBO;
+    glGenBuffers(1, &VBO); //访问OpenGL，让它分配给一个独一无二的ID
+    //OpenGl有很多缓存对象类型，顶点缓存对象的缓存类型是GL_ARRAY_BUFFER
+    //调用glBindBuffer把新创建的缓存绑定到GL_ARRAY_BUFFER目标上
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
+    //从此之后，我们使用的任何缓存调用都会用来配置当前绑定的缓冲。然后我们调用glBufferData函数，它会把之前定义的顶点数据复制到缓冲的内存中
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //第一个参数指定目标缓冲的类型：我们前面已经讲顶点数据绑定到了GL_ARRAY_BUFFER目标上
+    //第二个参数指定传输数据的大小
+    //第三个参数时我们希望发送的实际数据
+    //第四个参数指定显卡如何如何管理给定的数据
+    /*
+     他有三种形式：GL_STATIC_DRAW :数据不会或几乎不会改变
+     GL_DYNAMIC_DRAW:数据会被改变很多
+     GL_STREAM_DRAW:数据每次绘制时都会改变
+     */
+    
+    
+}
+
+
 -(void)render{
     
     glClearColor(0, 1, 1, 0);
@@ -155,20 +184,96 @@
     glViewport(0, 200, 200, 200);
     
     //使用glVertexAttribPointer函数告诉OpenGL该如何解析顶点数据（应用到逐个顶点属性上）
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), 0);
+    /*
+     第一个参数 指定我们要配置的顶点属性，我们在顶点在色器中 使用了layout(location = 0)定义了position顶点属性的位置，所以这个地方我们给的值时0
+     第二个参数 指定顶点属性的大小。顶点属性属性时一个vec3，它由3个值组成，所以大小是3
+     第三个参数指定数据的类型
+     第四个参数 我们是否希望数据被标准化。如果设置为GL_TRUE，所有数据都会被映射到0（有符号型signed数据是-1）到1之间。我们设置为GL_FALSE
+     第5个参数是步长。由于下个组位置数据在3个GLfloat之后，我们把步长设置为3 * sizeof(GLfloat)。要注意的是由于我们知道这个数组是紧密排列的（在两个顶点属性之间没有空隙）我们也可以设置为0来让OpenGL决定具体步长是多少（只有当数值是紧密排列时才可用）。一旦我们有更多的顶点属性，我们就必须更小心地定义每个顶点属性之间的间隔，我们在后面会看到更多的例子
+     最后一个参数的类型是GLvoid*，所以需要我们进行这个奇怪的强制类型转换。它表示位置数据在缓冲中起始位置的偏移量(Offset)。由于位置数据在数组的开头，所以这里是0。我们会在后面详细解释这个参数。
+     */
+    
+    glEnableVertexAttribArray(0);//启用顶点属性，默认是禁止的。
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
+    /*
+     glDrawArrays函数第一个参数是我们打算绘制的OpenGL图元的类型。由于我们在一开始时说过，我们希望绘制的是一个三角形，这里传递GL_TRIANGLES给它。第二个参数指定了顶点数组的起始索引，我们这里填0。最后一个参数指定我们打算绘制多少个顶点，这里是3（我们只从我们的数据中渲染一个三角形，它只有3个顶点长）
+    */
+    
+    [_context presentRenderbuffer:GL_RENDERBUFFER];
+
+}
+
+
+#pragma -mark 第一个三角形
+-(void)setupTriangle
+{
+    [self setupLayer];
+    [self setupContext];
+    [self setupBuffer];
+    [self setupVbo];
+    [self compileShaderVertex:@"VerterShader" fragment:@"FragmentShader"];
+    [self render];
+}
+
+-(GLuint)setVAO{
+    
+    /*
+     要想使用VAO，要做的只是使用glBindVertexArray绑定VAO。从绑定之后起，我们应该绑定和配置对应的VBO和属性指针，之后解绑VAO供之后使用。当我们打算绘制一个物体的时候，我们只要在绘制物体前简单地把VAO绑定到希望使用的设定上就行了。
+     */
+    
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    
+    //绑定VAO
+    glBindVertexArray(VAO);
+    
+    //把顶点数组复制到缓冲中供OpenGL使用
     
     GLfloat vertices[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
         0.0f,  0.5f, 0.0f
     };
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), vertices);
+    
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GL_FLOAT), 0);
     glEnableVertexAttribArray(0);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    [_context presentRenderbuffer:GL_RENDERBUFFER];
-
+    
+    //解绑VAO
+    glBindVertexArray(0);
+    /*
+    通常情况下当我们配置好OpenGL对象以后要解绑它们，这样我们才不会在其它地方错误地配置它们。
+    */
+    return VAO;
 }
 
+//使用VAO绘制三角形
+
+-(void)setupTriangleUseVAO
+{
+    [self setupLayer];
+    [self setupContext];
+    [self setupBuffer];
+    [self compileShaderVertex:@"VerterShader" fragment:@"FragmentShader"];
+    
+    glClearColor(0, 1, 1, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glViewport(0, 200, 200, 200);
+
+    glBindVertexArray([self setVAO]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    [_context presentRenderbuffer:GL_RENDERBUFFER];
+    
+    glBindVertexArray(0); //解绑
+
+}
 
 
 
@@ -177,11 +282,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         
-        [self setupLayer];
-        [self setupContext];
-        [self setupBuffer];
-        [self compileShaderVertex:@"VerterShader" fragment:@"FragmentShader"];
-        [self render];
+        [self setupTriangleUseVAO];
         
     }
     return self;
